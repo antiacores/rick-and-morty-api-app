2c66a456c5baa212cdd8ccce9bbff7ba8989ed64
Antía Cores <antiacores@MacBook-Air-de-Antia.local>
2024-10-16 18:54:17 -0600

    api consumption

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,14 @@
+import { Gallery } from "./components/Gallery"
+
 function App() {
 
   return (
     <>
-    <div className="min-h-screen bg-emerald-950 flex items-center justify-center">
+    <div className="min-h-screen bg-lime-950 flex flex-col items-center justify-center">
       <h1 className="text-3xl font-bold underline">
         RICK AND MORTY
       </h1>
+      <Gallery />
     </div>
     </>
   )
